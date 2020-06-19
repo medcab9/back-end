f6db33884ee9719b6ded9da2167dd03e1f66dda4
unknown <Dustin.Guillen14@yahoo.com>
2020-06-18 23:20:36 -0600

    removed displayed passwords

--- users/usersModel.js
+++ users/usersModel.js
@@ -10,7 +10,7 @@ module.exports = {
 };
 
 function find() {
-    return db('users').select('id', 'username', 'email', 'password')
+    return db('users').select('id', 'username', 'email')
 };
 
 function findBy(filter) {
