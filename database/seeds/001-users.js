6ce10fb45eec0ca4dd6c45187de936650c4e0fd4
unknown <Dustin.Guillen14@yahoo.com>
2020-06-22 15:55:11 -0600

    fixed seeds again

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -4,18 +4,18 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 901, username: 'CannabisKook', password: 'animal', email: 'hellokitty7757@gmail.com'},
-        {id: 902, username: 'wiseoldman', password: 'pickle', email: 'CaliChris@yahoo.com'},
-        {id: 903, username: 'Free_Byrd', password: 'tree', email: 'NateDiesel@yahoo.com'},
-        {id: 904, username: 'SexMetalBarbie420', password: 'frisbee', email: 'bigray122@hotmail.com'},
-        {id: 905, username: 'medicinalmelodies', password: 'korn', email: 'surfzen88@gmail.com'},
-        {id: 906, username: 'kimdavis3', password: 'monkey', email: 'Telizabeth713@HighGradeHealth.com'},
-        {id: 907, username: 'vet', password: 'peacock', email: 'G420man@gmail.com'},
-        {id: 908, username: 'BenchAdventures', password: 'pinon', email: 'Gree_house_420@gmail.com'},
-        {id: 909, username: 'PeaceSeeker', password: 'light', email: 'Leafthug400@LambdaSchool.com'},
-        {id: 910, username: 'paintergirl', password: 'guitar', email: 'liftedjoker@gmail.com'},
-        {id: 911, username: 'Chill_Panda', password: 'dolphin', email: 'ckoko1369@hotmail.com'},
-        {id: 912, username: 'Binkyfarms', password: '56463', email: 'bigray122@yahoo.com'},
+        { username: 'CannabisKook', password: 'animal', email: 'hellokitty7757@gmail.com' },
+        { username: 'wiseoldman', password: 'pickle', email: 'CaliChris@yahoo.com' },
+        { username: 'Free_Byrd', password: 'tree', email: 'NateDiesel@yahoo.com' },
+        { username: 'SexMetalBarbie420', password: 'frisbee', email: 'bigray122@hotmail.com' },
+        { username: 'medicinalmelodies', password: 'korn', email: 'surfzen88@gmail.com' },
+        { username: 'kimdavis3', password: 'monkey', email: 'Telizabeth713@HighGradeHealth.com' },
+        { username: 'vet', password: 'peacock', email: 'G420man@gmail.com' },
+        { username: 'BenchAdventures', password: 'pinon', email: 'Gree_house_420@gmail.com' },
+        { username: 'PeaceSeeker', password: 'light', email: 'Leafthug400@LambdaSchool.com' },
+        { username: 'paintergirl', password: 'guitar', email: 'liftedjoker@gmail.com' },
+        { username: 'Chill_Panda', password: 'dolphin', email: 'ckoko1369@hotmail.com' },
+        { username: 'Binkyfarms', password: '56463', email: 'bigray122@yahoo.com' },
       ]);
     });
 };
