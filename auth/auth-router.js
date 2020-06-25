10ab2b981252d8945c9115572a5944bf8362cfd6
unknown <Dustin.Guillen14@yahoo.com>
2020-06-25 12:41:03 -0600

    added if statement to login, check for username & password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const Users = require('../users/usersModel.js');
 const secrets = require('../api/secrets.js');
 
-// =========== POST User ===========
+// =========== REGISTER User ===========
 router.post('/register', (req, res) => {
     let user = req.body;
 
@@ -25,26 +25,31 @@ router.post('/register', (req, res) => {
         });
 });
 
-// =========== GET User by username ===========
+// =========== LOGIN User ===========
 router.post('/login', (req, res) => {
     let { username, password } = req.body;
+    if (req.body) {
+        Users.findBy({ username: username })
+            .then(([user]) => {
 
-    Users.findBy({ username })
-        .then(([user]) => {
+                if (user && bcrypt.compareSync(password, user.password)) {
 
-            if (user && bcrypt.compareSync(password, user.password)) {
+                    const token = generateToken(user);
 
-                const token = generateToken(user);
-
-                res.status(200).json({ message: 'Welcome!', token });
-            } else {
-                res.status(401).json({ message: 'You shall not pass!' });
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({ errorMessage: error.message });
+                    res.status(200).json({ message: `Welcome ${username}! You are user #${user.id}`, token });
+                } else {
+                    res.status(401).json({ message: 'username or password invalid' });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ errorMessage: error.message });
+            });
+    } else {
+        res.status(400).json({
+            message: "please provide email and password",
         });
+    }
 });
 
 // =========== Generate JSON Web Token ===========
