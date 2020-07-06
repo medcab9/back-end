0696d0a4d22b04737d8281cb6611f433ca9f0111
unknown <Dustin.Guillen14@yahoo.com>
2020-07-06 14:19:58 -0600

    added first tests

---
+++ __tests__/allTests.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+describe("all tests", () => {
+    it(": should be running tests", function () {
+        expect(true).toBeTruthy()
+    })
+
+        it.todo(": make more tests");
+    })
