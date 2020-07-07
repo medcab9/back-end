453199108dc66522f32d540eadac0b9f71d6ae79
unknown <Dustin.Guillen14@yahoo.com>
2020-07-06 18:04:50 -0600

    fixed test bug

--- __tests__/allTests.js
+++ __tests__/allTests.js
@@ -1,12 +1,16 @@
+const request = require('supertest')
+
+const db = require('../database/dbConfig');
+
+const server = require('../api/server');
 
 
 
 
-
-describe("all tests", () => {
+describe(`***************************************************************       All MedCab9 Tests        **************************************************************************************`, () => {
     it(": should be running tests", function () {
         expect(true).toBeTruthy()
     })
 
-        it.todo(": make more tests");
-    })
+    it.todo(": make more tests");
+})
--- knexfile.js
+++ knexfile.js
@@ -18,6 +18,18 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+
   staging: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
